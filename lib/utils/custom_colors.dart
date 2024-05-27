079a4f6c03d138eb1d3658e2439a96d9c2eada74
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-05-28 00:31:34 +0530

    colors

---
+++ lib/utils/custom_colors.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static Color get backGround => Color(0xFF121212);
+  static Color get appBar => Color(0xFF212121);
+  static Color get primary => Colors.blue;
+  static Color get white => Colors.white;
+}
